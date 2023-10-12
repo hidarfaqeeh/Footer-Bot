658524a0d6497efc0dfc2a9d22b8969b6b0ba24a
hidarfaqeeh <hidarfaqeeh@gmail.com>
2023-10-12 22:29:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ async def _start(bot: Client, m: Message):
             Config.START_TEXT,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("Support Group", url="https://t.me/DevsZone"), InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
-                    [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")]
+                    [InlineKeyboardButton("قناة البوت", url="https://t.me/zawamlAnsarAllah"), InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
+                    [InlineKeyboardButton("مطور البوت - @AKM100YE", url="https://t.me/AKM100YE")]
                 ]
             ),
             quote=True
